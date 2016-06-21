e721f7670e5861bbaa7e0052ebba997784ca9107
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-06-21 11:10:41 +0200

    using org-mode and org-gcal from git

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,2 +1,8 @@
+
+;; Org needs to be loaded at this stage!
+(add-to-list 'load-path "~/.emacs.d/plugins/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/plugins/org-mode/contrib/lisp" t)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
 ;;(load-file "~/.emacs.d/configuration.el")
