32e2ad112b85c0c4163fadbc3af26bfe3a81e64c
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-09-15 08:50:13 +0200

    emacs: ido avoid searches+column number;

--- scripts/SyncDoc.sh
+++ scripts/SyncDoc.sh
@@ -2,5 +2,6 @@
 
 
 #nmap 128.178.55.217 -PN -p ssh | grep open > /dev/null && (bash -c "unison Documents" && kdialog --passivepopup "Documents Synchronized" 5 || kdialog --passivepopup "There is some problems!" 5 ) || kdialog --passivepopup "Your computer is unreacheble!" 5
-nmap 128.178.55.217 -PN -p ssh | grep open > /dev/null && (bash -c "unison Documents" || kdialog --passivepopup "There is some problems!" 5 ) || kdialog --passivepopup "Your computer is unreacheble!" 5
+
+nmap 128.178.55.217 -PN -p ssh | grep open > /dev/null && (bash -c "unison Documents" && kdialog --passivepopup "Documents Synchronized" 5 || kdialog --passivepopup "There is some problems!" 5 ) || kdialog --passivepopup "Your computer is unreacheble!" 5
 
