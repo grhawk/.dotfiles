ccae8d712e90a1eaa6353635f202ca0accb04a16
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-09-26 15:42:51 +0200

    added rsync-ignore and something in startup

--- scripts/run_at_startup.sh
+++ scripts/run_at_startup.sh
@@ -5,3 +5,4 @@ xbindkeys -n -f .xbindkeysrc   # xbindkey as a daemon to get the keybind ;)
 tmux -2 new-session -n$USER -s $USER@$HOSTNAME -d -c "$HOME/" # Start tmux server
 (sleep 15; emacs -daemon)&		       # emacs will start much much faster
 
+./rslsync --webui.listen 127.0.0.1:8888 # Sync with raspberry
