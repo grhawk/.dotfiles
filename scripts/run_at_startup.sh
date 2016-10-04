ad9a2da30797c7b0a4d949bab2e7eb27edf52d59
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-10-04 14:57:14 +0200

    few modified stuff

--- scripts/run_at_startup.sh
+++ scripts/run_at_startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-(urxvtd -q -f -o)&                # urxvt server daemon to connect to with clients
-(xbindkeys -n -f .xbindkeysrc)&   # xbindkey as a daemon to get the keybind ;)
-(tmux -2 new-session -n$USER -s $USER@$HOSTNAME -d -c "$HOME/")& # Start tmux server
-(sleep 5; emacs -daemon)&		       # emacs will start much much faster
-(cd ~/.dotfiles/software/; ./rslsync; cd -)& # Sync with raspberry
+(urxvtd -q -f -o &>> /tmp/run_at_startup.log)&                # urxvt server daemon to connect to with clients
+(xbindkeys -n -f .xbindkeysrc &>> /tmp/run_at_startup.log)&   # xbindkey as a daemon to get the keybind ;)
+(tmux -2 new-session -n$USER -s $USER@$HOSTNAME -d -c "$HOME/" &>> /tmp/run_at_startup.log )& # Start tmux server
+(sleep 15; emacs -daemon &>> /tmp/run_at_startup.log)&		       # emacs will start much much faster
+(cd ~/.dotfiles/software/; ./rslsync &>> /tmp/run_at_startup.log; cd -)& # Sync with raspberry
