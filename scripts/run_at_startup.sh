d9570d52a0265293f1289620fc03de96796d9944
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2016-10-12 09:17:26 +0200

    Added some snippets

--- scripts/run_at_startup.sh
+++ scripts/run_at_startup.sh
@@ -4,4 +4,4 @@
 (xbindkeys -n -f .xbindkeysrc &>> /tmp/run_at_startup.log)&   # xbindkey as a daemon to get the keybind ;)
 (tmux -2 new-session -n$USER -s $USER@$HOSTNAME -d -c "$HOME/" &>> /tmp/run_at_startup.log )& # Start tmux server
 (sleep 15; emacs -daemon &>> /tmp/run_at_startup.log)&		       # emacs will start much much faster
-(cd ~/.dotfiles/software/; ./rslsync &>> /tmp/run_at_startup.log; cd -)& # Sync with raspberry
+(cd ~/.dotfiles/software/; ./rslsync ; cd -)& # Sync with raspberry
