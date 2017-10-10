f068157145f7d5bdb842c3636a6daa156e092a11
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2017-10-10 14:04:13 +0200

    [fix] the script was stuck in the loop

--- scripts/backup-ibm.bash
+++ scripts/backup-ibm.bash
@@ -4,21 +4,21 @@
 SSID=`networksetup -getairportnetwork en0 | awk '{print $(NF)}' || echo ""`
 NLOG=3
 
-
-
-
 export PATH="/Library/TeX/texbin:/usr/local/bin:/usr/local/bin:/Users/rpe/.dotfiles/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/TeX/texbin"
 export SHELL=/usr/local/bin/bash
 
 n_log=`ls -1 /tmp/dsmc.log.* 2>/dev/null | wc -l`
 while [[ "${n_log}" -lt "$NLOG" ]]; do
+  let n_log+=1
   touch /tmp/dsmc.log.${n_log}
   n_log=`ls -1 /tmp/dsmc.log.* 2>/dev/null | wc -l`
+#  echo "ASD $n_log"
 done
 
 oldest=`ls -1t /tmp/dsmc.log.* | tail -1 `
 
 
 if [[ "${SSID}" == "IBM" ]]; then
-  dsmc incr -detail &>"$oldest"
+#  echo "$oldest"
+  dsmc incr -detail &>"$oldest" &
 fi
