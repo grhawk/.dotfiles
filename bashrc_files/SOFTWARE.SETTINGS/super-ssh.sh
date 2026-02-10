5060880a6007c85b262876c6b00df7b111464a32
Riccardo Petraglia <riccardo.petraglia@gmail.com>
2026-02-10 17:05:51 +0100

    [bash] remove debug info

--- bashrc_files/SOFTWARE.SETTINGS/super-ssh.sh
+++ bashrc_files/SOFTWARE.SETTINGS/super-ssh.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-set -x
-
 ## super-ssh configuration
 export SUPER_SSH_TMUX_CONF="${HOME}/.dotfiles/tmux.conf"
 export SUPER_SSH_SESSION_MODE="per-host"
@@ -10,4 +8,3 @@ alias ssh="\${HOME}/.dotfiles/super-ssh/super-ssh"
 
 source "${HOME}/.dotfiles/super-ssh/completions/super-ssh.bash"
 
-set +x
